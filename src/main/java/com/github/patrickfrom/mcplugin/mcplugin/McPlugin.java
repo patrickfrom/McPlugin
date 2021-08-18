7cc1f1db426cca1f44b1ab78ea09789964f04205
StoreBjorn <85616505+vrcStoreBjorn@users.noreply.github.com>
2021-08-18 15:15:25 +0200

    Player join event

--- src/main/java/com/github/patrickfrom/mcplugin/mcplugin/McPlugin.java
+++ src/main/java/com/github/patrickfrom/mcplugin/mcplugin/McPlugin.java
@@ -1,12 +1,14 @@
 package com.github.patrickfrom.mcplugin.mcplugin;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.event.player;
 
 public final class McPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
@@ -14,4 +16,16 @@ public final class McPlugin extends JavaPlugin {
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @EventHandler
+    public void PlayerJoinEvent(Player playerJoined, String joinMessage) {
+
+    }
+
+    public void setJoinMessage() {
+
+    }
+
+
+
 }
