cda38ff361cce04e546d4f3fa9589420b1a31f98
StoreBjorn <85616505+vrcStoreBjorn@users.noreply.github.com>
2021-08-19 09:35:23 +0200

    Emerald block jump boost feature added

--- src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/PlayerEvents.java
+++ src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/PlayerEvents.java
@@ -2,13 +2,18 @@ package com.github.patrickfrom.mcplugin.mcplugin.scripts;
 
 import com.github.patrickfrom.mcplugin.mcplugin.McPlugin;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.Location;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class PlayerEvents implements Listener {
     private static final McPlugin plugin = McPlugin.getPlugin(McPlugin.class);
@@ -23,15 +28,20 @@ public class PlayerEvents implements Listener {
     }
 
     @EventHandler
+    public void PlayerEggThrowEvent(PlayerEggThrowEvent event) {
+        Player player = event.getPlayer();
+        player.chat("Hi");
+        player.sendMessage("Hello");
+    }
+
+
+    @EventHandler
     public void PlayerMoveEvent(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        Location location = event.getPlayer().getLocation();
-        location.setY(location.getY() -2);
+        PotionEffect effect = PotionEffect(PotionEffectType.JUMP, 1, 5);
 
-        int block = location.getBlockX();
-        if (block == 133) {
-            player.chat("hehe");
-            player.sendMessage("Hoho");
+        if(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == Material.EMERALD_BLOCK) {
+            player.addPotionEffect(effect);
         }
     }
 }
