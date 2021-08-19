84b6f4b8593b2f9e617de7d6fdb38289e9d704c7
StoreBjorn <85616505+vrcStoreBjorn@users.noreply.github.com>
2021-08-19 11:29:31 +0200

    Different boosts and stuff also scoreboards

--- src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/PlayerEvents.java
+++ src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/PlayerEvents.java
@@ -1,14 +1,17 @@
 package com.github.patrickfrom.mcplugin.mcplugin.scripts;
 
 import com.github.patrickfrom.mcplugin.mcplugin.McPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerEggThrowEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.Location;
@@ -16,6 +19,10 @@ import org.bukkit.potion.Potion;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.potion.PotionEffectTypeWrapper;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
 
 public class PlayerEvents implements Listener {
     private static final McPlugin plugin = McPlugin.getPlugin(McPlugin.class);
@@ -29,13 +36,25 @@ public class PlayerEvents implements Listener {
         player.sendMessage("Welcome " + player.getName());
     }
 
+    @EventHandler
+    public void PlayerInteractEntityEvent(PlayerInteractEntityEvent event) {
+        Player player = event.getPlayer();
+        Entity entity = event.getRightClicked();
+    }
+
     boolean jumpBoost = false;
     @EventHandler
     public void PlayerMoveEvent(PlayerMoveEvent event) {
         Player player = event.getPlayer();
-        PotionEffect effect = new PotionEffect(PotionEffectType.JUMP, 1, 5);
+        PotionEffect slowEffect = new PotionEffect(PotionEffectType.SLOW, 10, 3);
+        PotionEffect jumpEffect = new PotionEffect(PotionEffectType.JUMP, 10, 5);
+        PotionEffect speedEffect = new PotionEffect(PotionEffectType.SPEED, 10, 5);
         if (player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == Material.EMERALD_BLOCK) {
-            givePotionEffect(player, effect);
+            givePotionEffect(player, jumpEffect);
+        } else if (player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == Material.REDSTONE_BLOCK) {
+            givePotionEffect(player, speedEffect);
+        } else if (player.getLocation().getBlock().getRelative(BlockFace.DOWN).getType() == Material.COAL_BLOCK) {
+            givePotionEffect(player, slowEffect);
         }
     }
     public void givePotionEffect(Player player, PotionEffect effect) {
