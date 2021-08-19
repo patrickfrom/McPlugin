f4c7472772842399f8a90ddb6b25dd5cc36d95d0
patr9570 <patr9570@MININT-5HN3B0G>
2021-08-19 08:10:40 +0200

    Events Script

--- src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/Events.java
+++ src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/Events.java
@@ -1,15 +1,21 @@
 package com.github.patrickfrom.mcplugin.mcplugin.scripts;
 
 import com.github.patrickfrom.mcplugin.mcplugin.McPlugin;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class Events implements Listener {
     private static final McPlugin plugin = McPlugin.getPlugin(McPlugin.class);
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        event.setJoinMessage("Hello");
+        Player player = event.getPlayer();
+
+        event.setJoinMessage("");
+
+        player.sendMessage("Welcome " + player.getName());
     }
 }
