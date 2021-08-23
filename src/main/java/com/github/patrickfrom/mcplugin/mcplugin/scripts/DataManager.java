73190008fe6d8a6d4d102c91d713a8aaeb93013f
StoreBjorn <85616505+vrcStoreBjorn@users.noreply.github.com>
2021-08-23 08:04:05 +0200

    Why'd datamanager dissapear?!

---
+++ src/main/java/com/github/patrickfrom/mcplugin/mcplugin/scripts/DataManager.java
@@ -0,0 +1,7 @@
+package com.github.patrickfrom.mcplugin.mcplugin.scripts;
+
+public class DataManager {
+    protected String url = "jdbc:mysql://localhost:3306/minecraft?useSSL=false";
+    protected String user = "root";
+    protected String password = "RootMan123";
+}
